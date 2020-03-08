368260a7b01a6e677e0bcace4bd6ae62ac13139a
Atish Jain <atish_jain@yahoo.com>
2020-03-08 15:38:58 +0530

    Create Add2.c
    This Program is to add 2 numbers

---
+++ Add2.c
@@ -0,0 +1,24 @@
+/* Program name: Add2.c
+   Author: Atish Jain
+   IDE: C-Free 5.0
+   Task: Program to print Hello Message */
+   
+   # include <stdio.h>
+   main()
+   {
+     int a,b,c;
+     a=0;
+     b=0;
+     c=0;
+     
+     printf("Enter first no...:");
+     scanf("%d",&a);
+     
+     printf("Enter second no..:");
+     scanf("%d",&b);
+     
+     c=a+b;
+     
+     printf("The Result is....:%d",c);
+   	 printf("\n");
+   }
