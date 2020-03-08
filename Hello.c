2e43f1474ee802d1f822a91c80d15f8bdd66777b
Atish Jain <atish_jain@yahoo.com>
2020-03-08 15:33:18 +0530

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,11 @@
+/* Program name: Hello.c
+   Author: Atish Jain
+   IDE: C-Free 5.0
+   Task: Program to print Hello Message */
+   
+   # include <stdio.h>
+   main()
+   {
+   	 printf("Hello...Welcome");
+   	 printf("\n");
+   }
