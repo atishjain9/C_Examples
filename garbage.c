aacf3628fbb855c5fb8257780bf374710739ed6e
Atish Jain <atish_jain@yahoo.com>
2020-04-05 10:47:42 +0530

    Create garbage.c

---
+++ garbage.c
@@ -0,0 +1,15 @@
+/* Program name: garbage.c
+   Author: Atish Jain
+   Task: example on garbage value
+*/
+
+# include <stdio.h>
+main()
+{
+	int x=0;
+	int y;
+	
+	printf("\nx is:%d",x);
+	printf("\ny is:%d",y);
+	printf("\n");
+}
