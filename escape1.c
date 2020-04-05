ccc09fec135c8b17b89b66693ec8c92808bb5dd1
Atish Jain <atish_jain@yahoo.com>
2020-04-05 11:29:33 +0530

    Update escape1.c

--- escape1.c
+++ escape1.c
@@ -6,7 +6,49 @@
 # include <stdio.h>
 main()
 {
-  //printf("\a");
-  printf("Atish\aJain");
-  printf("\n");
+	// model 1: \n
+	printf("Welcome\n");
+	printf("Programmers");
+
+    printf("\n");
+	// model 2: \n
+	printf("Welcome");
+	printf("\nProgrammers");
+
+    printf("\n");	
+	
+	// model 2: \n
+	printf("Welcome\nProgrammers");
+
+
+    printf("\n");
+    // about \t
+	printf("Atish   Jain");	
+	printf("\nWelcome\tAtish");
+	printf("\nAtish\tJain");
+   
+   
+    printf("\n");
+    //about \b
+    printf("Atish\bJain");
+    printf("Atish\b\b\b\bJain");    
+
+    printf("\n");
+    //about \r
+    printf("Atish\rJain");
+    printf("Welcome\rAtish");    
+
+    printf("\n");
+    /* about \\ */
+    printf("\\n is used for new line");
+    
+    printf("\n");
+    //about \' and \"
+    printf("\nI Am Learning \"C Programming\" from \'Atish\' Jain Sir");
+
+    printf("\n");   
+    //about \a
+    printf("\nAtish\aJain");
+  
+   printf("\n");
 }
