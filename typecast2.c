1620f6f8e7c147d3c07788fd706edfac2715df33
Atish Jain <atish_jain@yahoo.com>
2020-04-05 11:04:38 +0530

    Create typecast2.c

---
+++ typecast2.c
@@ -0,0 +1,15 @@
+/* Program name:typecast2.c
+   Author: Atish Jain
+   Task: example on typecasting
+*/
+
+# include <stdio.h>
+main()
+{
+	int a;
+    char b='a';
+    a=b; // implicit conversion
+    printf("a=%d",a);
+
+	printf("\n");
+}
