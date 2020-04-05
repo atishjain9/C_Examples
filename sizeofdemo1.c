a63c67bef74860129c67173c2b8db43baae10401
Atish Jain <atish_jain@yahoo.com>
2020-04-05 10:56:19 +0530

    Create sizeofdemo1.c

---
+++ sizeofdemo1.c
@@ -0,0 +1,13 @@
+/* Program name:sizeofdemo1.c
+   Author: Atish Jain
+   Task:
+*/
+
+# include <stdio.h>
+main()
+{
+	
+	//printf("size of int is:%d",sizeof(int));
+	printf("size of long double is:%d",sizeof(long double));
+	printf("\n");
+}
