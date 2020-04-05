f24d0157d66bbf2279f0d9f2596b24e612dabee6
Atish Jain <atish_jain@yahoo.com>
2020-04-05 11:04:09 +0530

    Create typecast1.c

---
+++ typecast1.c
@@ -0,0 +1,16 @@
+/* Program name:typecast1.c
+   Author: Atish Jain
+   Task: example on typecasting
+*/
+
+# include <stdio.h>
+main()
+{
+	int a=5, b=2;
+	float c=0;
+	
+	c=(float)a/b;
+	
+	printf("%f",c);
+	printf("\n");
+}
