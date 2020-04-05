9babb993089a57a95d1b52978e2820deb50e0752
Atish Jain <atish_jain@yahoo.com>
2020-04-05 10:49:51 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+/* Program name:test.c
+   Author: Atish Jain
+   Task:
+*/
+
+# include <stdio.h>
+main()
+{
+	const int X=10;
+	const float Y=1.25;
+	const char Z='a';
+	const double D=12.50;
+	
+	printf("\n X is:%d",X);
+    printf("\n Y is:%f",Y);
+	printf("\n Z is:%c",Z);
+    printf("\n D is:%lf",D);	
+  
+  printf("\n");
+}
