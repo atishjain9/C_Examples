f3465f2715f75be7740137cb0674282dec682a28
Atish Jain <atish_jain@yahoo.com>
2020-04-05 10:53:53 +0530

    Create escape1.c

---
+++ escape1.c
@@ -0,0 +1,12 @@
+/* Program name:escape1.c
+   Author: Atish Jain
+   Task: Example on Escape sequence
+*/
+
+# include <stdio.h>
+main()
+{
+  //printf("\a");
+  printf("Atish\aJain");
+  printf("\n");
+}
