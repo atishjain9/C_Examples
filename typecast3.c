70510223c836d5f7657bd0c82ef1aa7d3ccb5dc8
Atish Jain <atish_jain@yahoo.com>
2020-04-05 11:05:11 +0530

    Create typecast3.c

---
+++ typecast3.c
@@ -0,0 +1,17 @@
+/* Program name:typecast3.c
+   Author: Atish Jain
+   Task: example on typecasting
+*/
+
+# include <stdio.h>
+main()
+{
+   int  i = 100;
+   char c = 'c'; 
+   int sum;
+
+   sum = i + c;
+   printf("Value of sum : %d\n", sum );
+
+   printf("\n");
+}
