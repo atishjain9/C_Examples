8c1d4be6c5b72f0abe737f94c023f8e6744a938f
Atish Jain <atish_jain@yahoo.com>
2021-04-03 21:35:42 +0530

    Add files via upload

---
+++ chardemo.c
@@ -0,0 +1,17 @@
+/* Program name: chardemo.c
+  Author: Atish Jain
+  IDE: C-Free 5.0
+  Task: Program to read a character the display the same */
+
+# include <stdio.h>
+main()
+{
+ char ch;
+ 
+ printf("Enter a character...:");
+ scanf("%c",&ch);
+ 
+ printf("\nThe Character is:%c",ch);
+
+printf("\n");
+}
