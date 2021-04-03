da9ad9c5a40dc9af4eb3087685697698f0ccc198
Atish Jain <atish_jain@yahoo.com>
2021-04-03 21:37:30 +0530

    Add files via upload

---
+++ strdemo.c
@@ -0,0 +1,16 @@
+/* Program name: strdemo.c
+  Author: Atish Jain
+  IDE: C-Free 5.0
+  Task: Program to read a string and display the same */
+
+# include <stdio.h>
+main()
+{
+ char name[100];
+ 
+ printf("Enter ur Name..:");
+ scanf("%s",&name);
+ 
+ printf("Heyyy %s, How are you....",name);
+printf("\n");
+}
