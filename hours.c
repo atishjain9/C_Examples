4fcd80d626d95e1f7d831f55ad38f79d84996bce
Atish Jain <atish_jain@yahoo.com>
2021-04-08 21:43:17 +0530

    Add files via upload

--- hours.c
+++ hours.c
@@ -1,18 +1,17 @@
 /* Program name: hours.c
-   Author: Atish Jain
-   IDE: C-Free 5.0
-   Task: Program to Convert Total Minutes into Hours and Minutes */
+  Author: Atish Jain
+  IDE: C-Free 5.0
+  Task: Program to Convert Total Minutes into Hours and Minutes */
+
 # include <stdio.h>
 main()
 {
 	int tm,h,m;
-	tm=h=m=0;
-	
-	printf("Enter total minutes:");
+	printf("Enter Total Minutes...:");
 	scanf("%d",&tm);
-	
 	h=tm/60;
 	m=tm%60;
-	
-	printf("Time is %d hrs and %d mins..\n",h,m);
+	printf("Heyy Time is %d hrs and %d mins....",h,m);
+	printf("\n");
+
 }
