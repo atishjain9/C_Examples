137e9667baa6db76b760d4b230e9a54a3f141965
Atish Jain <atish_jain@yahoo.com>
2021-04-08 21:29:47 +0530

    Add files via upload

--- ascex1.c
+++ ascex1.c
@@ -1,16 +1,17 @@
 /* Program name:ascex1.c
    Author: Atish Jain
-   Task: Read character input and print its ascii code.
+   Task: Read character input and print its ascii code
 */
 
 # include <stdio.h>
 main()
 {
-	char x;
-	printf("Enter any input:");
-	scanf("%c",&x);
+ char x;
+ 
+ printf("Enter an Input....:");
+ scanf("%c",&x);
+ 
+ printf("ASCII code of %c is %d",x,x);
 	
-	printf("ASCII code for %c is %d",x,x);
-	
-	printf("\n");
+ printf("\n");
 }
