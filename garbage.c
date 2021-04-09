706a6f02e7087ebed5372eaad8e90ebac8a3f839
Atish Jain <atish_jain@yahoo.com>
2021-04-09 14:52:13 +0530

    Add files via upload

--- garbage.c
+++ garbage.c
@@ -6,10 +6,36 @@
 # include <stdio.h>
 main()
 {
-	int x=0;
-	int y;
-	
-	printf("\nx is:%d",x);
-	printf("\ny is:%d",y);
-	printf("\n");
+ /*	
+ int x; // variable declaration
+ int y=0; //varaible declaration and initialization
+ 
+ printf("\nx is:%d",x);
+ printf("\ny is:%d",y);
+ 
+ x=10;
+ y=x*10+50;
+
+ printf("\nx is:%d",x);
+ printf("\ny is:%d",y);
+ */
+ 
+ /*
+ int a,b,c;
+ a=b=c=0;
+ printf("\na=%d",a);
+ printf("\nb=%d",b);
+ printf("\nc=%d",c);
+ */
+ 
+ unsigned long long int mno=919949128444;
+ float x=12.123456;
+ double y=12.123456789123459;
+ 
+ printf("\nmno=%llu",mno);
+ printf("\nx=%.6f",x);
+ printf("\ny=%.15lf",y);
+ 
+ 
+ printf("\n");
 }
