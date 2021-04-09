7eae932ec3c3319ffb6dc97912e51e17135b2d75
Atish Jain <atish_jain@yahoo.com>
2021-04-09 18:13:40 +0530

    Add files via upload

--- sizeofdemo.c
+++ sizeofdemo.c
@@ -1,24 +1,22 @@
 /* Program name:sizeofdemo.c
    Author: Atish Jain
-   Task: working with sizeof() function
+   Task: working with sizeof operator
 */
 
 # include <stdio.h>
 main()
 {
-	int x=10;
-	char ch='a';
-	float y=12.50;
-	double z=45.70;
-	
-	  printf("\nno of bytes occupied by int is:%d",sizeof(int));
-    printf("\nno of bytes occupied by x is:%d",sizeof(x));
-	  printf("\nno of bytes occupied by char is:%d",sizeof(char));
-    printf("\nno of bytes occupied by ch is:%d",sizeof(ch));
-	  printf("\nno of bytes occupied by float is:%d",sizeof(float));
-    printf("\nno of bytes occupied by y is:%d",sizeof(y));
-   	printf("\nno of bytes occupied by double is:%d",sizeof(double));
-    printf("\nno of bytes occupied by z is:%d",sizeof(z));
+ int x=10;
+ char ch='a';
+ float f=12.50;
+ double d=19.90;
+ 
+ printf("\nNo of Bytes Occupied by x is :%d",sizeof x);
+ printf("\nNo of Bytes Occupied by int is:%d",sizeof(int));	
 
-	printf("\n");
+ printf("\n\nNo of Bytes Occupied by ch is :%d",sizeof ch);
+ printf("\nNo of Bytes Occupied by char is:%d",sizeof(char));	
+  
+
+ printf("\n");
 }
