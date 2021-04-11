b7227dd917641a6c7b229c4047cecbe1a12896cc
Atish Jain <atish_jain@yahoo.com>
2021-04-11 16:36:25 +0530

    Add files via upload

---
+++ logicalerror.c
@@ -0,0 +1,18 @@
+/* Program name: logicalerror.c
+   Author: Atish Jain
+   Task:
+*/
+
+# include <stdio.h>
+main()
+{
+	int a,b,c;
+	a=50;
+	b=50;
+	c=50;
+	
+	printf("Average=%d",(a+b+c)/3);
+ 
+ 
+ printf("\n");
+}
