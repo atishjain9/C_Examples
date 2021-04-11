87d654fe7f935d8a316ba291370e42d22072b840
Atish Jain <atish_jain@yahoo.com>
2021-04-11 16:41:24 +0530

    Add files via upload

---
+++ typecast5.c
@@ -0,0 +1,20 @@
+/* Program name:typecast5.c
+   Author: Atish Jain
+   Task: example on typecasting
+*/
+
+# include <stdio.h>
+main()
+{
+ char ch='A';  //65
+ int a=100;   //100
+ short s=10;  //10
+ double d=5.5; //5.5
+ double res;
+ 
+ res=ch+a+s+d;
+ 
+ printf("\nres=%lf",res);
+ 
+ printf("\n");
+}
