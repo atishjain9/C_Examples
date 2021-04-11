1618e828e81fa3c411efdf9c9c7fe31d76ba04b3
Atish Jain <atish_jain@yahoo.com>
2021-04-11 16:35:15 +0530

    Add files via upload

--- errors.c
+++ errors.c
@@ -6,16 +6,8 @@
 # include <stdio.h>
 main()
 {
-	int no,d,res;
-	
-	printf("Enter a no:");
-	scanf("%d",&no);
-	
-	printf("Enter divider:");
-	scanf("%d",&d);
-	
-	res=no/d;
-	
-	printf("The Result is:%d",res);
-	printf("\n");
+ printf("Hello Welcome to Types of Errors....") // missing semicolon error
+ 
+ scanf("%d",&no);	  //undeclared error
+ printf("\n");
 }
