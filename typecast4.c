3ce750aa75870d64afb4b282d812c77ae633a9e6
Atish Jain <atish_jain@yahoo.com>
2021-04-11 16:41:06 +0530

    Add files via upload

--- typecast4.c
+++ typecast4.c
@@ -6,42 +6,12 @@
 # include <stdio.h>
 main()
 {
-	//case 1:
-	int x=10;
-	float y;
-	
-	y=x;
-	
-	printf("\n%f",y);
-	
-	//case 2:
-	float a=12.50;
-	int b=(int)a;
-	printf("\n%d",b);
-	
-	//case 3:
-	char ch='A';
-	int p=ch;
-	printf("\n%d",p);
-	
-	//case 4:
-	int q=100;
-	char c=q;
-	printf("\n%c",c);
-	
-	//case 5:
-	int s=353;
-	char z=s;
-	printf("\n%c",z);
-//--------------------------------------------
-
-
-const int DP=15;
-
-dis=amount*DP/100;
-
-
-
-
-   printf("\n");
+ int i=100;
+ char c='c';
+ int sum;
+ sum=i+c;
+ 
+ printf("\nSum of i and c is:%d\n",sum);
+ 
+ printf("\n");
 }
