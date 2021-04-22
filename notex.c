6a8b7ca0c3e6158dcd67469517f5cf21413bb915
Atish Jain <atish_jain@yahoo.com>
2021-04-22 13:54:10 +0530

    Add files via upload

---
+++ notex.c
@@ -0,0 +1,19 @@
+/* Program name:notex.c
+   Author: Atish Jain
+   Task: Example to work with not operator
+*/ 
+
+# include <stdio.h>
+main()
+{
+ int x=0;
+ 
+ printf("Enter a Non-Zero Value:");
+ scanf("%d",&x);
+ 
+ if(x!=0)
+  printf("WELCOME....");
+  
+ 
+ printf("\n");
+}
