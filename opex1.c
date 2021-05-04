b203d8016d5f44664a62e48deaac0d442fc4f67f
Atish Jain <atish_jain@yahoo.com>
2021-05-04 09:31:16 +0530

    Add files via upload

---
+++ opex1.c
@@ -0,0 +1,24 @@
+/* Program name: opex1.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on Operators*/
+
+# include <stdio.h>
+main()
+{
+	//printf("%d",10+5*2);
+	//printf("%d",1+2*3);
+	
+	//printf("%d",100/2*5);
+	//printf("%d",100*2/5);
+	//printf("%d",100+5/2);
+	
+	//printf("%d",(10/2)*5);
+	//printf("%d",10/(2*5));
+	//printf("%d",10+20/2-3*5);
+	
+	printf("%d",(10+2)*5);
+	
+  
+  printf("\n");
+}
