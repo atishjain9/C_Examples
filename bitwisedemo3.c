7cd67027c79f5c11873c9b328511595b276e23f7
Atish Jain <atish_jain@yahoo.com>
2021-05-04 10:36:57 +0530

    Add files via upload

---
+++ bitwisedemo3.c
@@ -0,0 +1,13 @@
+/* Program name: bitwisedemo3.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on bitwise NOT operator */
+   
+# include <stdio.h>
+main()
+{
+ int x=10;
+ printf("\n~10=%d",~x);
+ 
+ printf("\n");
+}
