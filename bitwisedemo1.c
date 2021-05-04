21c0d3ea9d4a343f713ed70ade27e1d0ac81b471
Atish Jain <atish_jain@yahoo.com>
2021-05-04 10:35:20 +0530

    Add files via upload

---
+++ bitwisedemo1.c
@@ -0,0 +1,17 @@
+/* Program name: bitwisedemo1.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on bitwise operators */
+   
+# include <stdio.h>
+main()
+{
+    int a=11;
+	int b=12;
+	
+	printf("\na&b = %d",a&b); //8
+	printf("\na|b = %d",a|b); //15
+	printf("\na^b = %d",a^b); //7
+ 
+  	printf("\n");
+}
