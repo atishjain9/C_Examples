fa4ca32af97c46557b2cfce38685d8956bfbe56b
Atish Jain <atish_jain@yahoo.com>
2021-05-04 10:36:06 +0530

    Add files via upload

---
+++ bitwisedemo2.c
@@ -0,0 +1,13 @@
+/* Program name: bitwisedemo2.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on bitwise Shift operators */
+   
+# include <stdio.h>
+main()
+{
+    int x=25;
+	printf("\nx>>2:%d",x>>2); //6
+	printf("\nx<<3:%d",x<<3); //200 
+  	printf("\n");
+}
