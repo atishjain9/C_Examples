41c91066fccc94cfc9304d9165a7e74ffd829fee
Atish Jain <atish_jain@yahoo.com>
2021-05-04 09:47:25 +0530

    Add files via upload

---
+++ unaryex3.c
@@ -0,0 +1,17 @@
+/* Program name: unaryex3.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on Operators*/
+
+# include <stdio.h>
+main()
+{
+ int a=1,b=0;
+ 
+ b=++a + --a;
+ 
+ printf("\na=%d",a);
+ printf("\nb=%d",b);
+ 
+  printf("\n");
+}
