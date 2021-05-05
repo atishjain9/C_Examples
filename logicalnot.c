0f26cd55889dd38cce88601a60fc9b4efed909e2
Atish Jain <atish_jain@yahoo.com>
2021-05-05 18:08:02 +0530

    Add files via upload

---
+++ logicalnot.c
@@ -0,0 +1,19 @@
+/* Program name: logicalnot.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Logical not Operator Example */
+   
+   # include <stdio.h>
+   main()
+   {
+   	
+    int x=100;
+    int y=-9;
+    int z=0;
+   
+    printf("\nx=%d",!x);
+    printf("\ny=%d",!y);
+    printf("\nz=%d",!z);
+
+    printf("\n");
+   }
