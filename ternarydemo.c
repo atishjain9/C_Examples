55d936d64d0055d35173d3a09fe15560abf25b3b
Atish Jain <atish_jain@yahoo.com>
2021-05-08 21:24:33 +0530

    Add files via upload

---
+++ ternarydemo.c
@@ -0,0 +1,18 @@
+/* Program name: ternarydemo.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on Ternary Operators */
+   
+# include <stdio.h>
+main()
+{
+	
+	//ex:1
+	//5>5 ? printf("Welcome"):printf("Bye");
+	//5-5 ? printf("Welcome"):printf("Bye");
+	
+	//ex:2
+	//10%2 ? printf("Red") : printf("Green");
+	10%3 ? printf("Red") : printf("Green");	
+  	printf("\n");
+}
