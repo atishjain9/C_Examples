bea92411625bf3ebbbb05b5d06a9d22874e0575a
Atish Jain <atish_jain@yahoo.com>
2021-05-08 21:28:03 +0530

    Add files via upload

---
+++ CompundAssignment.c
@@ -0,0 +1,18 @@
+/* Program name: compoundassignment.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Compound Assignment Example Example */
+   
+   # include <stdio.h>
+   main()
+   {
+    int x=10;
+    x=x*5+3;
+    printf("\n%d",x);  // 53
+ 
+    int y=10;
+    y*=5+3;
+    printf("\n%d",y);  // 80
+	
+    printf("\n");
+   }
