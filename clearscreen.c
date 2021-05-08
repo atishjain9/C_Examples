ca179c64f754b28dcdb79dc7ef9581b194b6b3fc
Atish Jain <atish_jain@yahoo.com>
2021-05-08 21:20:16 +0530

    Add files via upload

---
+++ clearscreen.c
@@ -0,0 +1,17 @@
+/* Program name: clearscreen.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to work with system() function to clear the screen */
+
+   # include <stdio.h>
+   # include <stdlib.h>
+   main()
+   {
+   	printf("Welcome to Coding Career Academy by Atish Jain");
+   	printf("\nPress any Key to Clear the Screen...");
+   	getch();
+   	
+   	system("cls");
+   	
+    printf("\n");
+   }
