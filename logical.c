bec85a70d5f40e90bed00f24b1e3ea8de203d81d
Atish Jain <atish_jain@yahoo.com>
2021-05-16 09:19:28 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,63 @@
+/* Program name: logical.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Example on Logical Loop */
+   
+   # include <stdio.h>
+   main()
+   {
+     int no=1;
+     
+     /*
+     //v-1
+     while(1)
+     {
+     	printf("\n%d",no);
+     	no++;
+     	
+     	if(no>10)
+     	  break;
+     }
+     printf("\nEnd of the Loop...");
+   	*/	 
+   	
+   	/*
+   	//v-2
+     while(-90)
+     {
+     	printf("\n%d",no);
+     	no++;
+     	
+     	if(no>10)
+     	  break;
+     }
+     printf("\nEnd of the Loop...");
+     */
+     
+     /*
+     //v-3
+     while(no)
+     {
+     	printf("\n%d",no);
+     	no++;
+     	
+     	if(no>10)
+     	  break;
+     }
+     printf("\nEnd of the Loop...");
+     
+     */
+     
+     //v-4
+     while(0)
+     {
+     	printf("\n%d",no);
+     	no++;
+     	
+     	if(no>10)
+     	  break;
+     }
+     
+     printf("\nEnd of the Loop...");
+     printf("\n");
+   }
