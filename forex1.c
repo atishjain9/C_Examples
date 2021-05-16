ed7ebfb5897d0fb895d964d8d3be521fd140e6c8
Atish Jain <atish_jain@yahoo.com>
2021-05-16 09:12:07 +0530

    Add files via upload

---
+++ forex1.c
@@ -0,0 +1,18 @@
+/* Program name: forex1.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to print "Hello World" 10 times */
+   
+   # include <stdio.h>
+   main()
+   {
+  	int count;
+  	
+  	for(count=1;count<=10;count++)
+       printf("\n%d. Hello World",count);
+       
+    printf("\nEnd of the Loop....");   
+    
+   	 
+     printf("\n");
+   }
