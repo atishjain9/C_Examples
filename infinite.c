c5eac633d221542a0805a9effd65e109ba64c5c6
Atish Jain <atish_jain@yahoo.com>
2021-06-01 18:13:54 +0530

    Add files via upload

---
+++ infinite.c
@@ -0,0 +1,18 @@
+/* Program name: infinite.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : program to work with infinite loop */
+
+   # include <stdio.h>
+   main()
+   {
+   	
+   	for(;;)
+   	{
+     system("color 1");
+	 printf("\tAtish Jain");   	
+    }
+     
+   	printf("\n");
+   }
+
