f92eb9938823d48391240c2dcf5c7789bc41bbda
Atish Jain <atish_jain@yahoo.com>
2021-06-06 20:24:23 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,29 @@
+/* Program name: Pattern1.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to Print Patterns using Starts and Numbers */
+
+# include <stdio.h>
+main()
+{
+ int i,j,r;
+ 
+ printf("Enter How Many Rows...:");
+ scanf("%d",&r);
+ 
+ for(i=1;i<=r;i++)
+ {
+ 	for(j=1;j<=r;j++)
+ 	{
+	 	if(j<=i)
+	 	  printf(" * ");
+	 	else
+		  printf("   ");  
+	 }
+	 printf("\n");
+ }
+
+ printf("\n");
+}
+
+
