b24a34ccd20c24ad25a56c952a121ebb0af96fbf
Atish Jain <atish_jain@yahoo.com>
2021-06-08 16:43:23 +0530

    Add files via upload

---
+++ numpattern1.c
@@ -0,0 +1,25 @@
+/* Program name: numpattern1.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to Print Patterns using Numbers   */
+
+# include <stdio.h>
+main()
+{
+ int i,j,no;
+ 
+ printf("Enter How Many Rows....:");
+ scanf("%d",&no);
+ 
+ for(i=1;i<=no;i++)
+ {
+ 	for(j=1;j<=i;j++)
+ 	{
+	 	printf("%d ",j);
+	 }
+	 printf("\n");
+ }
+  printf("\n");
+}
+
+
