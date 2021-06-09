d5ca6afb64f36119a9bcc566f70dc4ba2a13d9b3
Atish Jain <atish_jain@yahoo.com>
2021-06-09 14:54:46 +0530

    Add files via upload

---
+++ textpat3.c
@@ -0,0 +1,41 @@
+/* Program name: textpat3.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to Print Patterns using Alphabets   */
+
+# include <stdio.h>
+main()
+{
+ int i,j,tar;
+ char ch1,ch2;
+ ch1='A';
+ 
+ printf("Enter ur Target Alphabet...(A-Z):");
+ scanf("%c",&ch2);
+ 
+ if(!((ch2>=65 && ch2<=90) ||(ch2>=97 && ch2<=122)))
+ {
+ 	printf("Invalid Input...");
+ 	exit(0);
+ }
+ 
+ ch2=toupper(ch2);
+     
+         
+ tar=ch2-ch1;
+ tar=tar+1;
+ 
+ for(i=1;i<=tar;i++)
+ {
+ 	for(j=1;j<=i;j++)
+ 	{
+	 	printf("%c ",ch1);
+	 }
+	 printf("\n");
+	 ch1++;
+ }
+ 
+ 
+ printf("\n");
+}
+
