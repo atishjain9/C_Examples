4deb088a7dfb9b027f9bdedd0798d545709caf2e
Atish Jain <atish_jain@yahoo.com>
2021-06-09 14:51:36 +0530

    Add files via upload

---
+++ textpat1.c
@@ -0,0 +1,33 @@
+/* Program name: textpat1.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to Print Patterns using Alphabets   */
+
+# include <stdio.h>
+main()
+{
+ int i,j,tar;
+ char ch='A';
+ 
+ printf("Enter How Many Rows You Want to Print(Max-26):");
+ scanf("%d",&tar);
+ 
+ if(!(tar>=1 && tar<=26))
+ {
+ 	printf("Invalid input....");
+ 	exit(0);
+ }
+ 
+ for(i=1;i<=tar;i++)
+ {
+ 	for(j=1;j<=i;j++)
+ 	{
+	 	printf("%c ",ch);
+	 }
+	ch++; 
+	printf("\n");
+ }	
+ printf("\n");
+}
+
+
