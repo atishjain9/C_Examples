8a910ab3f6c6b4539ecda23212824959fba4d0f5
Atish Jain <atish_jain@yahoo.com>
2021-07-22 09:32:55 +0530

    Add files via upload

---
+++ fundecldemo1.c
@@ -0,0 +1,18 @@
+/* Program name:funcdecldemo1.c
+   Author: Atish Jain
+   Task: When Function not returning any value not even void
+*/
+
+# include <stdio.h>
+
+main()
+{
+ display(); //function call
+ printf("\n");
+}
+
+
+display() //function definition
+{
+  putchar('A');
+}
