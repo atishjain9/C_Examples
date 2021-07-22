dac360b0602bcded8d06c22655a68d3613b0a834
Atish Jain <atish_jain@yahoo.com>
2021-07-22 09:42:46 +0530

    Add files via upload

---
+++ mainfunc.c
@@ -0,0 +1,28 @@
+/* Program name: mainfunc.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Variations of main functions */
+
+# include <stdio.h>
+
+//model 1
+main()
+{
+ printf("Hello.....");
+}
+
+//model 2
+/*void main()
+{
+ printf("Hello.....");
+}*/
+
+//model 3
+/*int main()
+{
+ printf("Hello.....");
+ return 0;
+}*/
+
+	    
+	    
