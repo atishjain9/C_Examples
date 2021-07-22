434844e54166bc3f228a8a0a41fae22114e8b0f4
Atish Jain <atish_jain@yahoo.com>
2021-07-22 09:34:03 +0530

    Add files via upload

---
+++ fundecldemo2.c
@@ -0,0 +1,18 @@
+/* Program name:funcdecldemo2.c
+   Author: Atish Jain
+   Task: when the function is returning int value
+*/
+
+# include <stdio.h>
+
+main()
+{
+ printf("ASCII Code of A is:%d",display()); //function call
+ printf("\n");
+}
+
+
+int display() //function definition
+{
+  return('A');
+}
