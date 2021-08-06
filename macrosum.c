51cb58bfc9b83d18b811a04ab2af8900f14bad74
Atish Jain <atish_jain@yahoo.com>
2021-08-06 21:49:55 +0530

    Add files via upload

---
+++ macrosum.c
@@ -0,0 +1,26 @@
+/* Program name:macrosum.c
+   Author: Atish Jain
+   Task: Program to work with macros
+*/
+
+# include <stdio.h>
+# define pf printf("Enter a no...:");
+# define sf scanf("%d",&
+main()
+{
+ int a,b;
+ a=b=0;
+ 
+ pf
+ sf a);
+ 
+ pf
+ sf b);
+ 
+ printf("Sum of Two numbers is:%d",a+b);
+ printf("\n");
+}
+
+
+
+
