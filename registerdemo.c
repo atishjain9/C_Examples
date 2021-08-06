259e6f4fe1eab1b457152945c5920d021a1e17d7
Atish Jain <atish_jain@yahoo.com>
2021-08-06 09:05:39 +0530

    Add files via upload

---
+++ registerdemo.c
@@ -0,0 +1,22 @@
+/* Program name:registerdemo.c
+   Author: Atish Jain
+   Task: demo on register variables
+*/
+
+# include <stdio.h>
+
+main()
+{
+ register int i=1;
+ 
+ while(i<=100)
+ {
+  printf("\tI Love C");
+  i=i+1;	
+ } 
+ 
+ 
+ printf("\n");
+}
+
+
