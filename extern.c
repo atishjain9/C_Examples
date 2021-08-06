06ecd0e719cc4f7caab0d9855065a23e0a38a888
Atish Jain <atish_jain@yahoo.com>
2021-08-06 08:52:53 +0530

    Add files via upload

---
+++ extern.c
@@ -0,0 +1,6 @@
+//Declare all the required variables and access using extern keyword in any program
+
+char name[]="Coding Career Academy";
+char course[]="Coding Carrer BluePrint";
+const float fees=25000;
+const int duration = 90;
