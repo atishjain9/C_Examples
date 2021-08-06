1b7d9c757373140ee5bfc574e01cfac3f75a5626
Atish Jain <atish_jain@yahoo.com>
2021-08-06 09:19:43 +0530

    Add files via upload

---
+++ autodemo2.c
@@ -0,0 +1,22 @@
+/* Program name:autodemo2.c
+   Author: Atish Jain
+   Task: demo on auto storage class variables
+*/
+
+# include <stdio.h>
+main()
+{
+ display();
+ display();
+ display();
+ printf("\n");
+}
+
+display()
+{
+	int x=10;
+	printf("\nx is:%d",x);
+	x++;
+}
+
+
