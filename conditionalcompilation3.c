bdba79f1dc775a26fd4224a9ed669dbe68b72370
Atish Jain <atish_jain@yahoo.com>
2021-08-06 22:01:03 +0530

    Add files via upload

---
+++ conditionalcompilation3.c
@@ -0,0 +1,15 @@
+/* Program name: conditionalcompilation3.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : Program to Work with Conditional Compilation*/
+
+//#include <stdio.h>
+
+#ifndef NULL
+  #define NULL "Welcome to Coding Career Academy...."
+#endif
+main()
+{
+ printf("%s",NULL);
+ printf("\n");
+}
