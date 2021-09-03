5a441fe0c4520a29d5d618696225856482c58cb2
Atish Jain <atish_jain@yahoo.com>
2021-09-03 10:42:16 +0530

    Add files via upload

---
+++ address1.c
@@ -0,0 +1,13 @@
+/* Program name:address1.c
+   Author: Atish Jain
+   Task: Example on & Operator
+*/
+
+# include <stdio.h>
+
+main()
+{
+ int x=100;
+ printf("Value of x is %d and Address of x is %u",x,&x);	
+  printf("\n");
+}
