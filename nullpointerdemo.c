d962047a30fb836d00ca7fb9e7b65222984bd8c2
Atish Jain <atish_jain@yahoo.com>
2021-09-03 10:44:40 +0530

    Add files via upload

---
+++ nullpointerdemo.c
@@ -0,0 +1,14 @@
+/* Program name: nullpointerdemo.c
+   Author : Atish Jain
+   IDE: C-Free 5.0
+   Task : example on null pointers */
+
+# include <stdio.h>
+main()
+{
+ void *ptr=NULL;
+ 
+ printf("ptr is:%u",ptr);
+   
+   printf("\n");
+}
